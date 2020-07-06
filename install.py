11768392601f0d974c6a11069e007636a152bf26
AnithaRamachandranR <53323724+AnithaRamachandranR@users.noreply.github.com>
2020-07-06 00:01:48 -0700

    Create install.py

---
+++ install.py
@@ -0,0 +1,25 @@
+import os
+def fun(data):
+    #print(data)
+    try:
+        print(data)
+        __import__(data)
+        #importlib.import_module(data)
+        
+       # os.system('import {}'.format(data))
+    except ImportError:
+        print ("Trying to Install required module: {}\n".format(data))
+        print('python -m pip install {}'.format(data))
+        os.system('python -m pip install {}'.format(data))
+f = open("app.py", "r")
+for i in f:
+   if "from" in i:
+         list_of_words = i.split('from')
+         word = list_of_words[1].split('import')
+         data=word[0].strip()
+         print(data)
+   else:
+    word= i.split('import')
+    data=word[1].strip()
+    print(data)
+   fun(data)
