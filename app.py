84946934b5efdc191b4aea17fb00e46f0dc45b6f
AnithaRamachandranR <53323724+AnithaRamachandranR@users.noreply.github.com>
2020-07-05 23:59:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 
 from flask import Flask
-import subprocess
-import sys
+import boto3
+#import subprocess
+#import sys
 app = Flask(__name__)
-reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
-installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
-print(installed_package)
+#reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+#installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+#print(installed_package)
 @app.route("/ani")
 def index():
     return "Hello, world!"
