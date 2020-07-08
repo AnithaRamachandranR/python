1b1157199503504fe1725d85905f8d5d4c3f3011
AnithaRamachandranR <53323724+AnithaRamachandranR@users.noreply.github.com>
2020-07-08 05:39:02 -0700

    Create test1.py
    check the local and s3 file

---
+++ test1.py
@@ -0,0 +1,36 @@
+import boto3
+import os
+from os import listdir
+from os.path import isfile, join
+ACCESS_KEY = 'AKIAJEJD4WFA24VOBHYA'
+SECRET_KEY = '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'
+s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY)
+mypath="/home/ec2-user/anitha/"
+buck_obj=[]
+filename=[]
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+for i in onlyfiles:
+    if i.endswith(".txt"):
+        filename.append(i)
+data=s3.list_objects_v2(Bucket='anitha-bucket')
+for i in data['Contents']:
+    buck_obj.append(i['Key'])
+#li_u_removed = [i.encode('utf-8') for i in buck_obj]
+#print("bucket",buck_obj)
+#print(li_u_removed)
+#print(filename,buck_obj)
+"""data=set(filename)-set(buck_obj)
+list_data=list(data)
+if len(list_data)>0:
+    return 1
+else :
+    return 0"""
+#print(filename,buck_obj)
+try:
+    for files in filename:
+        if files not in buck_obj:
+            raise "Error"
+    print("0")
+except :
+    print("1")
+#    return 0
